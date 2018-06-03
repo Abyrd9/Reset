3c13184ced551119baa754142b23cfa33507ffcc
Andrew Byrd <andrew.byrd99@gmail.com>
2018-06-03 17:44:01 -0400

    created Quote Block component

---
+++ src/js/components/common/QuoteBlock.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const QuoteBlock = props => {
+	return (
+		<div className="quote-block">
+			<i className="fas fa-quote-left quote-block__icon"></i>
+			{props.quote}
+			<i className="fas fa-quote-right quote-block__icon"></i>
+		</div>
+	);
+};
+
+QuoteBlock.propTypes = {
+	quote: PropTypes.string
+};
+
+export default QuoteBlock;
